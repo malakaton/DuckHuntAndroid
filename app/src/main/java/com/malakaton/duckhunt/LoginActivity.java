5cfcb4b78b5c861643c78fa43f3c5d72403f6eec
Manel Bermudez <manel.bermudez@credimarket.com>
2019-09-16 18:28:42 +0200

    Login Activity => code added GameActivity added, design

--- app/src/main/java/com/malakaton/duckhunt/LoginActivity.java
+++ app/src/main/java/com/malakaton/duckhunt/LoginActivity.java
@@ -2,13 +2,50 @@ package com.malakaton.duckhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.malakaton.duckhunt.common.Constantes;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText etNick;
+    Button btnStart;
+    String nick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etNick = findViewById(R.id.editTextNick);
+        btnStart = findViewById(R.id.buttonStart);
+
+        // Eventos: evento click
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nick = etNick.getText().toString();
+
+                if (nick.isEmpty()) {
+                    etNick.setError("El nombre de usuario es obligatorio");
+                } else {
+                    /**
+                     * Pasamos de un activity a otro, es decir, pasaremos del login a la pantalla
+                     * del juego, el objeto intent recibe como primer parametro el activity
+                     * en el que estamos y el segundo parametro el activity que queremos ir
+                     *
+                     * Para lanzar el cambio de activity hay que llamar al metodo startActivity
+                     *
+                     */
+                    Intent i = new Intent(LoginActivity.this, GameActivity.class);
+                    // Pasar parametros de un activity a otro (pantalla)
+                    i.putExtra(Constantes.EXTRA_NICK, nick);
+                    startActivity(i);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/malakaton/duckhunt/common/Constantes.java
@@ -0,0 +1,5 @@
+package com.malakaton.duckhunt.common;
+
+public class Constantes {
+    public static final String EXTRA_NICK = "nickname";
+}
